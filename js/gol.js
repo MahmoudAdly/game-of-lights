c3c8609a2c7c8648d1f12c49eaa922be4a5c9bf6
mahmoud adly <mahmoud.adly@gmail.com>
2015-03-11 01:04:34 +0200

    add calculations algorithm

--- js/gol.js
+++ js/gol.js
@@ -2,6 +2,7 @@ var Game = {
   background: null,
   cellSize: 64,
   ledsArray: [[]],
+  newStatesArray: [[]],
   colorsArray: ['r','g','b'],
   active: false,
   timerObject: null,
@@ -11,7 +12,7 @@ var Game = {
     Game.loadAssets();
     Game.createLeds();
     Game.createStartButton();
-    // Crafty.bind('ViewportResize', this.adaptToScreen);
+    Crafty.bind('ViewportResize', Game.resizeScreen);
     Crafty.trigger('ViewportResize');
   },
   loadAssets: function() {
@@ -40,49 +41,77 @@ var Game = {
       button_stop:[0,0]
     });
   },
-  adaptToScreen: function() {
-    console.log('w: ' + Crafty.viewport._width + ', h: ' + Crafty.viewport._height);
-    // change background size
-    Game.background.attr({w: Crafty.viewport._width, h: Crafty.viewport._height});
+  resizeScreen: function() {
+    /* This will RESET the game */
   },
   createLeds: function() {
     // calculate needed cells
     var cols = Math.floor(Crafty.viewport._width / Game.cellSize);
     var rows = Math.floor(Crafty.viewport._height / Game.cellSize);
-    console.log('cols: ' + cols + ', rows: ' + rows);
-    // fill new array with data from old array, or create new items
+
+    // populate the array with newly created leds
     for(var i=0; i<rows; i++) {
       if(Game.ledsArray[i] == undefined) Game.ledsArray.push([]);
       for(var j=0; j<cols; j++) {
         Game.ledsArray[i][j] = Game.newLed(i,j);
       }
     }
+
+    // populate another array to use in calculations.
+    // this is better that regenarating it at every cycle
+    for(var i = 0; i < Game.ledsArray.length; i++) {
+      if(Game.newStatesArray[i] == undefined) Game.newStatesArray.push([]);
+      for(var j = 0; j < Game.ledsArray[i].length; j++) {
+        Game.newStatesArray[i][j] = false;
+      }
+    }
   },
   newLed: function(row, col) {
+    // select led color randomly and set the sprite component accordingly
     var randColor = Game.colorsArray[Crafty.math.randomInt(0,Game.colorsArray.length-1)];
     var img_component = randColor + "_off";
-    var e = Crafty.e('2D, Canvas, Mouse, LED')
+    /*
+    Collision box : 
+    1- Set the collision box to be bigger than the led to use it for
+    neighbourhood calculations. But not too big to overlap with the far neighbour cell.
+    2- Polygon points are relative to the parent entity.
+    */
+    var e = Crafty.e('2D, Canvas, Mouse, Collision')
                 .attr({x: Game.cellSize*col, 
                   y: Game.cellSize*row, 
                   w: Game.cellSize, 
                   h: Game.cellSize,
                   ledColor: randColor,
                   ledOn: false})
+                .collision(new Crafty.polygon([-(Game.cellSize*0.25),-(Game.cellSize*0.25)], 
+                                              [(Game.cellSize*1.25),-(Game.cellSize*0.25)],
+                                              [(Game.cellSize*1.25),(Game.cellSize*1.25)],
+                                              [-(Game.cellSize*0.25),(Game.cellSize*1.25)]))
                 .addComponent(img_component)
                 .bind('Click', function(e) {
                   if( e.mouseButton == Crafty.mouseButtons.LEFT ) {
+                    // invert led state
                     Game.setLed(this, !this.ledOn);
                   }
                 });
-    e.origin('center').attr({rotation: Crafty.math.randomInt(-45,45)});
+    /*
+    Disable rotation as it will mess with collition boxes. Add WiredHitBox component 
+    to the created object above to see the intersections mess.
+    */
+    // e.origin('center').attr({rotation: Crafty.math.randomInt(-45,45)});
+    
     return e;
   },
   setLed: function(led, newState) {
     newState = (typeof(newState)==='boolean') ? newState : false ;
     if(newState == false) {
-      led.removeComponent(led.ledColor+"_on").addComponent(led.ledColor+"_off");
+      led.removeComponent(led.ledColor+"_on")
+          .addComponent(led.ledColor+"_off")
+          .removeComponent('ON');
     } else {
-      led.removeComponent(led.ledColor+"_off").addComponent(led.ledColor+"_on");
+      led.removeComponent(led.ledColor+"_off")
+          .addComponent(led.ledColor+"_on")
+          .addComponent('ON');
     }
     led.ledOn = newState;
   },
@@ -90,8 +119,42 @@ var Game = {
     for (var i = 0; i < Game.ledsArray.length; i++) {
       for (var j = 0; j < Game.ledsArray[i].length; j++) {
         Game.setLed(Game.ledsArray[i][j], false);
-      };
-    };
+      }
+    }
+  },
+  calculate: function() {
+    /*
+    For each led, check the collision with the leds with ON state,
+    then do the Conway calculations accordingly.
+    */
+    for (var i = 0; i < Game.ledsArray.length; i++) {
+      for (var j = 0; j < Game.ledsArray[i].length; j++) {
+        // get led collisions count
+        var led = Game.ledsArray[i][j];
+        var neighbours = led.hit('ON');
+        if(neighbours == false) var neighboursCount = 0;
+        else var neighboursCount = neighbours.length;
+        
+        // apply Conway's rules
+        switch(neighboursCount) {
+          case 2:
+            if(led.ledOn == true) Game.newStatesArray[i][j] = true;
+            else Game.newStatesArray[i][j] = false;
+            break;
+          case 3:
+            Game.newStatesArray[i][j] = true;
+            break;
+          default:
+            Game.newStatesArray[i][j] = false;
+        }
+      }
+    }
+    // apply new states on leds
+    for (var i = 0; i < Game.ledsArray.length; i++) {
+      for (var j = 0; j < Game.ledsArray[i].length; j++) {
+        Game.setLed(Game.ledsArray[i][j], Game.newStatesArray[i][j]);
+      }
+    }
   },
   createStartButton: function() {
     var e = Crafty.e('2D, Canvas, Mouse')
@@ -115,14 +178,12 @@ var Game = {
                 });
   },
   start: function() {
-    console.log('start game');
     Game.timerObject = setInterval(function() {
-      console.log('recalculate grid');
+      Game.calculate();
     }, Game.timerInterval);
     Game.active = true;
   },
   stop: function() {
-    console.log('stop game');
     clearInterval(Game.timerObject);
     Game.resetLeds();
     Game.active = false;
